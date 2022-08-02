4acaaf3abbc604fa110f24243753fd6b420f1ff2
S.Kenurshan <49789464+Kenurshan@users.noreply.github.com>
2022-08-02 13:07:00 +0530

    Create enc.py

---
+++ enc.py
@@ -0,0 +1,11 @@
+import random
+
+def main():
+    encoded_string = "灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彥㜰㍢㐸㙽"
+    for i in range(len(encoded_string)):
+        print(chr(ord(encoded_string[i])>>8))
+        print(chr((ord(encoded_string[i]))-((ord(encoded_string[i])>>8)<<8)))
+        print (i)
+
+
+main()
